de409a30d10132e27e95e018369ace04922e54b7
mukolakutsil <mukola.kutsil22@gmail.com>
2020-11-27 19:10:56 +0200

    add_Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import style from './Header.module.css';
+
+
+const Header = (props) => {
+    return (
+        <header>
+            <ul>
+                <li>Список</li>
+                <li>Додати</li>
+            </ul>
+        </header>
+    )
+};
+
+export default Header;
